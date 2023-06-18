3791f5a8b34679c768425faff2efe50183f510ca
Max Guclu <m.guclu@live.se>
2023-06-18 21:56:29 +0200

    Adjusted some names and files. Added some css

--- React-Vite-Api/src/App.jsx
+++ React-Vite-Api/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PersonPage from "./pages/PersonPage";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/person" element={<PersonPage />} />
